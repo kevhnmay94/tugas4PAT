9f6fa1de488e40f57d1ab299d3eae8d549933418
adwisatya <a.dwisaty4@yahoo.com>
2015-10-09 00:06:19 +0700

    Refactoring package

--- RabbitMQLearning/src/tugas4pat/RabbitMQClient.java
+++ RabbitMQLearning/src/tugas4pat/RabbitMQClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tutorial3;
+package tugas4pat;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
